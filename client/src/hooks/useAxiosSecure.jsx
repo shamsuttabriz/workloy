f45dc06c5d2052617a18f77f510e0a807e4256e5
shamsuttabriz <shamsuttabriz.md@gmail.com>
2025-10-25 13:16:13 +0600

    server link add in the hook section

--- client/src/hooks/useAxiosSecure.jsx
+++ client/src/hooks/useAxiosSecure.jsx
@@ -3,8 +3,8 @@ import useAuth from "./useAuth";
 import { useNavigate } from "react-router";
 
 const axiosSecure = axios.create({
-  // baseURL: `https://server-seven-tau-43.vercel.app`,
-  baseURL: `http://localhost:5001`,
+  baseURL: `https://workloy-server.vercel.app`,
+  // baseURL: `http://localhost:5001`,
 });
 
 const useAxiosSecure = () => {
--- client/src/hooks/userAxios.jsx
+++ client/src/hooks/userAxios.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  // baseURL: `https://server-seven-tau-43.vercel.app`,
-  baseURL: `http://localhost:5001`,
+  baseURL: `https://workloy-server.vercel.app`,
+  // baseURL: `http://localhost:5001`,
 });
 
 const userAxios = () => {
